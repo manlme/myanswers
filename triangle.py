5ea54236eff670236d2819c43025bb8e4fc3b522
jiangdaye <mosquito.blood@gmail.com>
2017-01-17 17:26:27 +0800

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+
+def tell_triangle(a,b,c):
+    precision = 0.00000001
+    if a+b > c and a+c> b and b+c>a:
+        pass
+    else:
+        return 'not a triangle'
+    if a == b == c:
+        return "equilateral"
+    elif abs(a-b) < precision or abs(a-c) < precision or abs(b-c) < precision:
+        return  "isosceles"
+    else:
+        return "scalene"
